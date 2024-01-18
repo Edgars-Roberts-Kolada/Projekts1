6835a7eb5a66045b644d5ac0d08448a15a182ee3
Edgars-Roberts-Kolada <144727120+Edgars-Roberts-Kolada@users.noreply.github.com>
2024-01-18 04:14:24 +0000

    Skaitums un visi komentāri.

--- Projekts.py
+++ Projekts.py
@@ -6,24 +6,26 @@ from reportlab.lib.units import cm
 from reportlab.pdfgen.canvas import Canvas
 import os
 
+
+# E-pata atversana
 imap_url = 'imap.gmail.com'
 my_mail = imaplib.IMAP4_SSL(imap_url)
-my_mail.login("erk.testpasts@gmail.com", "nzvy jpdl mjcm gcjh")
-
+my_mail.login("erk.testpasts@gmail.com", "nzvy jpdl mjcm gcjh") #Testesanas e-pasts, kuru es dzesisu, kad tiks izlikts vertejums. Savaa versijaa ir mana galvenaa e-pata adrese un parole.
 my_mail.select("INBOX", readonly=True)
+aaa, data = my_mail.search(None, 'ALL')
 
-aaa, data = my_mail.search(None, 'ALL')  
 
+# Palaisanas laika nolasisana un saglabasana
 Lastrun = datetime.datetime.strptime(open("lastrun.txt","r").read(), "%d-%m-%Y (%H:%M:%S)")
 
-Tagadsafe = datetime.datetime.now() - datetime.timedelta(hours=1)
+Tagadsafe = datetime.datetime.now() - datetime.timedelta(hours=1) #Nezinu vai ekstra stunda daudz ko mainis, bet jutas drosaak gadijumam ja e-pasts antnaak skripta palaisanas laikaa.
 
 open("lastrun.txt","w+").write((Tagadsafe).strftime("%d-%m-%Y (%H:%M:%S)"))
 
 
 
-
-list = reversed(data[0].split())
+# Jauno e-pastu sutitaja, temata un laika ievietosana listaa
+list = reversed(data[0].split()) #Parveide lai e-patsus parbauditu sakot ar jaunaako
 temp=[]
 pasti=[]
 for i in list:
@@ -31,7 +33,7 @@ for i in list:
     mail=data[0][1]
     msg = email.message_from_bytes(mail)
     Date=email.utils.parsedate_to_datetime(msg['Date']).replace(tzinfo=None)
-    if Lastrun > Date:
+    if Lastrun > Date:  #Parbaude vai epasts jau bija pajajusajaa palaisanaa
         break
     temp.append(email.utils.parseaddr(msg['from'])[1])
     temp.append(msg['subject'])
@@ -41,10 +43,12 @@ for i in list:
 
 
 
-
+# Vecaa atgaadinaajuma dzesana
 if os.path.exists("Atgadinajums.pdf"):
   os.remove("Atgadinajums.pdf")
 
+
+# Atgadinajuma pdf veidosana, ja ir jauni e-pasti
 if pasti:
     canvas = Canvas("Atgadinajums.pdf", pagesize=(40 * cm, 18 * cm))
     canvas.setFont("Times-Roman", 40)
@@ -59,5 +63,7 @@ if pasti:
 
     canvas.save()
 
+
+# Atgadinajuma atversana, ja ir jauni e-pasti
 if os.path.exists("Atgadinajums.pdf"):
   os.system("Atgadinajums.pdf")
